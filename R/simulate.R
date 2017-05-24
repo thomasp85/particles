4d8eff9331a2c69af7e9d3e7010b4b48e9296567
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-05-24 13:05:08 +0200

    Add node setup

--- R/simulate.R
+++ R/simulate.R
@@ -1,7 +1,13 @@
 #' @importFrom tidygraph as_tbl_graph
+#' @export
 simulate <- function(graph, ...) {
+  graph <- as_tbl_graph(graph)
+  universe <- create_universe(...)
+  genesis <- init_particles(universe, graph)
   structure(list(
-    particles = as_tbl_graph(graph),
-    universe = create_universe(...)
-  ), class = 'particle_system')
+    particles = graph,
+    position = genesis$position,
+    velocity = genesis$velocity,
+    universe = universe
+  ), class = 'simulation')
 }
--- R/universe.R
+++ R/universe.R
@@ -1,11 +1,46 @@
-create_universe <- function(...) {
+#' @export
+create_universe <- function(alpha = 1, alpha_min = 0.001,
+                            alpha_decay = 1- alpha_min^(1/300), alpha_target = 0,
+                            velocity_decay = 0.6,
+                            setup = default_genesis(), ...) {
   structure(list(
-    parameters = list(...),
-    genesis = list(),
-    forces = list()
+    parameters = list(
+      alpha = alpha,
+      alpha_min = alpha_min,
+      alpha_decay = alpha_decay,
+      alpha_target = alpha_target,
+      velocity_decay = velocity_decay,
+      ...
+    ),
+    genesis = setup,
+    forces = list(),
+    constraints = list()
   ), class = 'universe')
 }
+#' @export
+is.universe <- function(x) inherits(x, 'universe')
+
+#' @importFrom igraph gorder
+#' @export
+default_genesis <- function(radius = 10, angle = pi * (3 - sqrt(5))) {
+  function(particles, ...) {
+    n_particles <- gorder(particles)
+    p_radius <- radius * sqrt(seq_len(n_particles))
+    p_angle <- angle * seq_len(n_particles)
+    pos <- cbind(p_radius * cos(p_angle), p_radius * sin(p_angle))
+    vel <- matrix(0, ncol = 2, nrow = n_particles)
+    list(position = pos, velocity = vel)
+  }
+}
+
+# Helpers -----------------------------------------------------------------
+
+init_particles <- function(universe, particles) {
+  universe$genesis(particles, universe$parameters)
+}
 add_force <- function(universe, force) {
+  stopifnot(is.universe(universe))
+  stopifnot(is.force(force))
   universe$forces <- append(universe$forces, list(force))
   universe
 }
