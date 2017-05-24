33c16417e6fd858726dd3130117b1bc4bd43eef2
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-05-24 13:05:43 +0200

    Add base force class

---
+++ R/force.R
@@ -0,0 +1,20 @@
+#' @export
+is.force <- function(x) inherits(x, 'force')
+train_force <- function(force, particles, ...) {
+  UseMethod('train_force')
+}
+train_force.default <- function(force, particles, ...) {
+  stop('The provided object is not a force', call. = FALSE)
+}
+train_force.force <- function(force, particles, ...) {
+  stop('The provided force does not implement a training method', call. = FALSE)
+}
+apply_force <- function(force, particles, pos, vel, alpha, ...) {
+  UseMethod('apply_force')
+}
+apply_force.default <- function(force, particles, pos, vel, alpha, ...) {
+  stop('The provided object is not a force', call. = FALSE)
+}
+apply_force.force <- function(force, particles, pos, vel, alpha, ...) {
+  stop('The provided force does not implement an apply method', call. = FALSE)
+}
