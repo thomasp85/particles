9af6a22d909b8c9f87e8a234e7a62ffd8d986f4f
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-02-10 18:01:04 +0100

    Allow center to be applied to individual nodes

--- R/center_force.R
+++ R/center_force.R
@@ -12,14 +12,27 @@
 #' @format NULL
 #' @export
 center_force <- structure(list(
+  x = NULL,
+  x_quo = NULL,
+  y = NULL,
+  y_quo = NULL,
+  center_groups = NULL
 ), class = c('center_force', 'force'))
 #' @export
 print.center_force <- function(x, ...) {
   cat('Center Force:\n')
   cat('* A force that keeps all particles centered around the origin\n')
 }
-train_force.center_force <- function(force, particles, ...) {
+train_force.center_force <- function(force, particles, x = NULL, y = NULL, ...) {
   force <- NextMethod()
+  force$x_quo <- enquo(x)
+  force$y_quo <- enquo(y)
+  nodes <- as_tibble(particles, active = 'nodes')
+  x <- eval_tidy(force$x_quo, nodes) %||% 0
+  y <- eval_tidy(force$y_quo, nodes) %||% 0
+  force$x <- rep(x, length.out = nrow(nodes))
+  force$y <- rep(y, length.out = nrow(nodes))
+  force$center_groups <- split(seq_len(nrow(nodes)), paste0(force$x, '-', force$y))
   force
 }
 #' @importFrom rlang quos
@@ -28,13 +41,21 @@ retrain_force.center_force <- function(force, particles, ...) {
   dots <- quos(...)
   particle_hash <- digest(particles)
   new_particles <- particle_hash != force$particle_hash
-  force$particle_hash <- particle_hash
-  nodes <- as_tibble(particles, active = 'nodes')
-  force <- update_quo(force, 'include', dots, nodes, new_particles, TRUE)
+  if (new_particles) {
+    force$particle_hash <- particle_hash
+    nodes <- as_tibble(particles, active = 'nodes')
+    force <- update_quo(force, 'include', dots, nodes, new_particles, TRUE)
+    force <- update_quo(force, 'x', dots, nodes, new_particles, 0)
+    force <- update_quo(force, 'y', dots, nodes, new_particles, 0)
+    force$center_groups <- split(seq_len(nrow(nodes)), paste0(force$x, '-', force$y))
+  }
   force
 }
 apply_force.center_force <- function(force, particles, pos, vel, alpha, ...) {
-  center <- matrix(colMeans(pos), nrow = nrow(pos), ncol = ncol(pos), byrow = TRUE)
-  pos <- pos - center
+  for (i in force$center_groups) {
+    adjust <- colMeans(pos[i, , drop = FALSE]) - c(force$x[i[1]], force$y[i[1]])
+    center <- matrix(adjust, nrow = length(i), ncol = ncol(pos), byrow = TRUE)
+    pos[i, ] <- pos[i, , drop = FALSE] - center
+  }
   list(position = pos, velocity = vel)
 }
