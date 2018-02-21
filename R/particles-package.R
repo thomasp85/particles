5291f5162e01f2e493331260f9fb851edcdfc082
Thomas Lin Pedersen <thomasp85@gmail.com>
2018-02-21 22:24:54 +0100

    Better package docs

--- R/particles-package.R
+++ R/particles-package.R
@@ -1,4 +1,7 @@
 #' @useDynLib particles
 #' @importFrom Rcpp sourceCpp
-#' @aliases NULL
+#' @aliases particles_package
+#'
+#' @references See also the [GitHub page](https://github.com/d3/d3-force) for
+#' the original JavaScript implementation in D3 by Mike Bostock
 '_PACKAGE'
